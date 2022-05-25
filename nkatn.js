2369d7ba060ff3b7b99e3e2d62f8bb59ab898fb0
arttimofee <artm.timofeev.08@mail.ru>
2022-05-25 20:11:24 +1000

    add game (past)

--- nkatn.js
+++ nkatn.js
@@ -1,88 +1,72 @@
-var cvs = document.getElementById("canvas");
-var ctx = cvs.getContext("2d"); 
+var cvs = document.getElementById("canvas")
+var vid = cvs.getContext("2d")
 
-var bird   = new Image (); 
+var bird = new Image();
 var bg = new Image();
 var fg = new Image();
 var pipeUp = new Image();
-var pipeBottom = new Image();
-
+var pipeDown = new Image();
 
 bird.src = "img/bird.png";
 bg.src = "img/bg.png";
 fg.src = "img/fg.png";
-pipeBottom.src = "img/pipeBottom.png";
 pipeUp.src = "img/pipeUp.png";
+pipeDown.src = "img/pipeBottom.png";
 
-var gap = 90;
+var gap = 90
 
-document.addEventListener("keydown", moveUp); 
+document.addEventListener("keyspace", moveUp)
 
-function moveUp(){
-	yPos -= 20;
-	fly.play();
+function moveUp() {
+    yPos -= 20
+    fly.play()
 }
 
+ var pipe = []
+ pipe[0] = {
+     x: cvs.width,
+     y: 0
+ }
 
+ var xPos = 10
+ var yPos = 150
+ var grav = 1
+ var score = 0
 
-var pipe = []; 
-pipe[0] = {
-	x : cvs.width,
-	y : 0
-}
+ function draw() {
+     vid.drawImage(bg,0,0)
 
+     for(i=0;i<pipe.length;i++) {
+         vid.drawImage(pipeUp, pipe[i].x, pipe[i].y)
+         vid.drawImage(pipeDown, pipe[i].x, pipe[i].y + pipeUp.height + gap)
+         pipe[i].x--
 
+         if(pipe[i].x == 125) {
+             pipe.push({
+                x: cvs.width,
+                y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+             })
+         }
+         if (xPos +bird.width >= pipe[i].x
+            && xPos <= pipe[i].x + pipeUp.width
+            && (yPos <= pipe[i].y + pipeUp.height || yPos + bird.height >= pipe[i].y + pipeUp.height+gap)
+            && (yPos <= pipe[i].y + pipeUp.height || yPos + bird.height >= pipe[i].y + cvs.height - fg.height)){
+                location.reload();
+          }
+          if (pipe[i].x == 5) {
+            score++;
+            score_audio.play();
+        }
+     }
+     
+     vid.drawImage(fg,0,cvs.height - fg.height)
+     vid.drawImage(bird,xPos,yPos)
+     
+     yPos += grav
+     vid.fillStyle ="#000"
+     vid.font = "24px"
+     vid.fillText("Счёт: " + score, 10, cvs.height - 20)
+     requestAnimationFrame(draw)
+ }
 
-var fly = new Audio();
-var score_audio = new Audio();
-
-fly.src = "audio/fly.mp3";
-score_audio.src = "audio/score.mp3";
-
-
-var xPos = 10;
-var yPos = 150;
-var grav = 1;
-var score = 0;
-
-function draw(){
-	ctx.drawImage(bg,0,0);
-
-	for(var i =0; i< pipe.length; i++){ 
-	ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
-    ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
-    pipe[i].x--;
-
-    if(pipe[i].x==125) {
-    	pipe.push({  
-    		x : cvs.width,
-    		y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
-    	});
-    }
-
-    if (xPos +bird.width >= pipe[i].x
-    	&& xPos <= pipe[i].x + pipeUp.width
-    	&& (yPos <= pipe[i].y + pipeUp.height || yPos + bird.height >= pipe[i].y + pipeUp.height+gap)
-    	&& (yPos <= pipe[i].y + pipeUp.height || yPos + bird.height >= pipe[i].y + cvs.height - fg.height)){
-    		location.reload();
-      }
-      if (pipe[i].x == 5) {
-		score++;
-		score_audio.play();
-	   }
-	}
-
-
-    
-    ctx.drawImage(fg, 0, cvs.height - fg.height);
-    ctx.drawImage(bird, xPos, yPos);
-
-    yPos += grav; 
-    ctx.fillStyle = "#000";
-    ctx.font = "24px";
-    ctx.fillText("Счет: " + score, 10, cvs.height - 20);
-    requestAnimationFrame(draw);
-}
-
-
-pipeBottom.onload = draw;
+ pipeDown.onload = draw
